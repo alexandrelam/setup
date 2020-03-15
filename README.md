96a463213cadedf7e1bf9c4946c842f078cfc7f4
Alexandre <mrnivclones@gmail.com>
2020-03-15 12:10:15 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Setup Ubuntu 
+
+### Prérequis
+
+Ce script fonctionne sur **Ubuntu 18.04 et supérieur.**
+
+### Installation
+
+Rendre le fichier executable : `sudo chmod +x install.sh`
+
+Executer le fichier : `./install.sh`
+
+### Logiciels installés
+
+* Google-Chrome
+* Youtube-DL
+* Mpv
+* SublimeText
+* Install Pip for python
+* Uninstall python2 make python 3 default
+* Install GIT
---
+++ install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+echo "This script will install the following :"
+echo "Update and upgrade"
+echo "Google Chrome"
+echo "Youtube-DL"
+echo "Mpv"
+echo "sublimetext"
+echo "install pip for python"
+echo "uninstall python2 make python 3 default"
+echo "install git"
+echo ""
+echo ""
+echo ""
+
+
+sudo apt update
+sudo apt -y upgrade
+
+#install google chrome
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt update
+sudo apt -y install google-chrome-stable
+
+#install youtube-dl
+sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
+sudo chmod a+x /usr/local/bin/youtube-dl
+hash -r
+sudo youtube-dl -U
+
+#install mpv
+sudo add-apt-repository -y ppa:mc3man/mpv-tests
+sudo apt -y install mpv
+
+
+#install sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt -y install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt -y update
+sudo apt -y install sublime-text
+
+#uninstall python2.7
+sudo apt -y purge python2.7-minimal
+
+#install pip
+sudo apt -y install python3-pip
+
+#make python 3 default
+sudo update-alternatives  --set python /usr/bin/python3.6
+
+#install git
+sudo apt -y install git
+
+echo "This script will install the following :"
+echo "Update and upgrade"
+echo "Google Chrome"
+echo "flatpak"
+echo "Discord"
+echo "Youtube-DL"
+echo "Mpv"
+echo "sublimetext"
+echo "install pip for python"
+echo "uninstall python2 make python 3 default"
+echo "install git"
